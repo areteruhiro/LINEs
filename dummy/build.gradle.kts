db9d64339c4b3c7b94777c69764d29a1a4097cc9
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-02-24 05:16:17 +0900

    Update build.gradle.kts

--- dummy/build.gradle.kts
+++ dummy/build.gradle.kts
@@ -1,13 +1,9 @@
 plugins {
     id("java")
-      id("app.revanced.library.patches")    
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(11))
     }
-}
-dependencies {
-    implementation("app.revanced:revanced-patcher:17.0.0")
 }
