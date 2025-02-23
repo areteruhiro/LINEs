ebf8b483154dad6f0e13ee51b5160e6399da07cb
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-02-24 03:36:33 +0900

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,14 +1,12 @@
-group = "app.revanced"
+group = "app.LIMEs"
 
 patches {
     about {
-        name = "ReVanced Patches template"
-        description = "Patches template for ReVanced"
-        source = "git@github.com:revanced/revanced-patches-template.git"
+        name = "ReVanced Patches LIMEs"
+        description = "Patches LIMEs for ReVanced"
+        source = "git@github.com:areteruhiro/LINEs.git"
         author = "ReVanced"
-        contact = "contact@revanced.app"
-        website = "https://revanced.app"
+        contact = "limebeta.dev@gmail.com"
         license = "GNU General Public License v3.0"
     }
 }
-
