adb96030488f7d896def5a4687b13f7896254450
areteruhiro <narumihiroki1114@gmail.com>
2025-02-24 04:32:09 +0900

    テスト

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("app.revanced.patches")
-    id("org.jetbrains.kotlin.jvm")
+    id("app.revanced.library.patches") version "17.0.0"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
 }
 
 dependencies {
-    implementation("app.revanced:revanced-patcher:1.0.0-dev.5")
+    implementation("app.revanced:revanced-patcher:17.0.0")
     implementation("org.jf.dexlib2:dexlib2:2.5.2")
     implementation("org.smali:smali:2.5.2")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.77")
@@ -13,12 +13,12 @@ dependencies {
 
 patches {
     about {
-        name = "ReVanced Patches LIMEs"
+        name = "LINE ReVanced Patches"
         description = "LINE Modification Enhancements for ReVanced"
         source = "https://github.com/areteruhiro/LINEs"
         author = "LIME Team"
         contact = "limebeta.dev@gmail.com"
         website = "https://limes.example.com"
-        license = "GNU General Public License v3.0"
+        license = "GPL-3.0"
     }
 }
