a382051f37043963ff18e7526b90264a1a55a973
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-02 17:20:42 +0900

    Update ExamplePatch.kt

--- src/main/src/main/kotlin/app/revanced/patches/line/ExamplePatch.kt
+++ src/main/src/main/kotlin/app/revanced/patches/line/ExamplePatch.kt
@@ -1,72 +1,56 @@
-package app.revanced.patches.line
+package app.revanced.patches.line.misc.gmscore
 
-import app.revanced.patcher.patch.Option
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchException
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.line.utils.extension.hooks.applicationInitHook
 import app.revanced.patches.shared.gms.gmsCoreSupportPatch
 import app.revanced.patches.shared.compatibility.Constants.COMPATIBLE_PACKAGE
-import app.revanced.patches.shared.settings.ResourceUtils.updatePackageName
-import app.revanced.patches.shared.settings.ResourceUtils.updatePatchStatus
-import app.revanced.patches.shared.settings.settingsPatch
-import app.revanced.util.valueOrThrow
 
-// LINEのMainActivityフィンガープリントを直接宣言
-internal val mainActivityFingerprint = legacyFingerprint(
-    name = "lineMainActivityFingerprint",
-    returnType = "V",
-    parameters = listOf("Landroid/os/Bundle;"),
-    strings = listOf(
-        "android.intent.action.MAIN",
-        "FEline_home"
-    ),
-    customFingerprint = { method, classDef ->
-        method.name == "onCreate" && classDef.endsWith("Activity;")
-    }
-)
-
-// LINE用の拡張パッチを直接宣言
-val sharedExtensionPatch = object {
-    // LINE用のapplicationInitHook
-    val applicationInitHook = app.revanced.patches.line.utils.extension.hooks.applicationInitHook
-
-    // LINE用のcronetEngineContextHook
-    val cronetEngineContextHook = app.revanced.patches.shared.extension.hooks.cronetEngineContextHook
-
-    // LINE用のfirebaseInitProviderContextHook
-    val firebaseInitProviderContextHook = app.revanced.patches.shared.extension.hooks.firebaseInitProviderContextHook
-
-    // LINE用のmainActivityBaseContextHook
-    val mainActivityBaseContextHook = app.revanced.patches.line.utils.extension.hooks.mainActivityBaseContextHook
-}
-
-@Suppress("unused")
-val gmsCoreSupportPatch = gmsCoreSupportPatch(
-    fromPackageName = "jp.naver.line.android", // LINEのパッケージ名
-    mainActivityOnCreateFingerprint = mainActivityFingerprint.second, // LINEのMainActivityフィンガープリント
-    extensionPatch = sharedExtensionPatch, // LINE用の拡張パッチ
-    gmsCoreSupportResourcePatchFactory = ::gmsCoreSupportResourcePatch,
-) {
-    compatibleWith(COMPATIBLE_PACKAGE) // 互換性のあるパッケージを指定
-}
-
-private fun gmsCoreSupportResourcePatch(
-    gmsCoreVendorGroupIdOption: Option<String>,
-    packageNameLineOption: Option<String>
-) = app.revanced.patches.shared.gms.gmsCoreSupportResourcePatch(
-    fromPackageName = "jp.naver.line.android", // LINEのパッケージ名
-    spoofedPackageSignature = "e682fe0bcd60907dfed515e0b8a4de03aa1c281d111a07833986602b6098afd2", // LINEの正規署名を指定
-    gmsCoreVendorGroupIdOption = gmsCoreVendorGroupIdOption,
-    packageNameYouTubeOption = packageNameLineOption, // LINEのパッケージ名を使用
-    packageNameYouTubeMusicOption = packageNameLineOption, // LINEのパッケージ名を使用
-    executeBlock = {
-        updatePackageName(
-            gmsCoreVendorGroupIdOption.valueOrThrow() + ".android.gms", // GMS CoreのベンダーグループIDを更新
-            packageNameLineOption.valueOrThrow() // LINEのパッケージ名を更新
+@Patch(
+    name = "LINE GMS Core Support",
+    description = "Google Mobile Services (GMS) サポートを有効化",
+    dependencies = [
+        gmsCoreSupportPatch::class, // 依存パッチ
+        settingsPatch::class
+    ],
+    compatiblePackages = [
+        CompatiblePackage(
+            "jp.naver.line.android",
+            ["12.0.0", "13.5.0"] // 対応バージョン範囲
         )
-
-        updatePatchStatus("GMSCORE_SUPPORT") // GMSCORE_SUPPORTのステータスを更新
-    },
+    ]
+)
+class LineGmsCoreSupportPatch : BytecodePatch(
+    fingerprints = listOf(mainActivityFingerprint)
 ) {
-    dependsOn(
-        settingsPatch, // 必要な依存関係を追加
-        sharedExtensionPatch // LINE用の拡張パッチを依存関係に追加
-    )
+    override fun execute(context: BytecodeContext) {
+        // 1. メインアクティビティのフック設定
+        val mainActivityClass = context.findClass(mainActivityFingerprint)
+            ?: throw PatchException("MainActivity not found")
+
+        // 2. GMS Core 初期化処理の挿入
+        mainActivityClass.methods.first { it.name == "onCreate" }.apply {
+            addInstructions(
+                0,
+                """
+                    invoke-static {p0}, ${applicationInitHook.declaringClass}->initialize(Landroid/content/Context;)V
+                """
+            )
+        }
+
+        // 3. リソースの更新
+        updatePackageName("custom.gms.provider")
+        updatePatchStatus("GMS_ENABLED")
+    }
+
+    companion object {
+        // フィンガープリントの共有化
+        val mainActivityFingerprint = methodFingerprint(
+            returnType = "V",
+            parameters = listOf("Landroid/os/Bundle;"),
+            strings = listOf("FEline_home", "MainActivity")
+        )
+    }
 }
