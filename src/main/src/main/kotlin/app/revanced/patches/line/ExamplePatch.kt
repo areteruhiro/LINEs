780923cecb90134a31db20d44923d351f886f049
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-02 10:47:16 +0900

    Update ExamplePatch.kt

--- src/main/src/main/kotlin/app/revanced/patches/line/ExamplePatch.kt
+++ src/main/src/main/kotlin/app/revanced/patches/line/ExamplePatch.kt
@@ -8,37 +8,46 @@ import app.revanced.patches.shared.settings.ResourceUtils.updatePatchStatus
 import app.revanced.patches.shared.settings.settingsPatch
 import app.revanced.util.valueOrThrow
 
+// LINEのMainActivityフィンガープリントを直接宣言
+val mainActivityFingerprint = object {
+    val second = "LINEのMainActivityフィンガープリントに関する情報" // 実際のフィンガープリント情報をここに設定
+}
+
+// LINE用の拡張パッチを直接宣言
+val sharedExtensionPatch = object {
+    // ここにLINE用の拡張パッチの実装を追加
+}
+
 @Suppress("unused")
 val gmsCoreSupportPatch = gmsCoreSupportPatch(
     fromPackageName = "jp.naver.line.android", // LINEのパッケージ名
-    mainActivityOnCreateFingerprint = mainActivityFingerprint.second,
-    extensionPatch = sharedExtensionPatch,
+    mainActivityOnCreateFingerprint = mainActivityFingerprint.second, // LINEのMainActivityフィンガープリント
+    extensionPatch = sharedExtensionPatch, // LINE用の拡張パッチ
     gmsCoreSupportResourcePatchFactory = ::gmsCoreSupportResourcePatch,
 ) {
-    compatibleWith(COMPATIBLE_PACKAGE)
+    compatibleWith(COMPATIBLE_PACKAGE) // 互換性のあるパッケージを指定
 }
 
 private fun gmsCoreSupportResourcePatch(
     gmsCoreVendorGroupIdOption: Option<String>,
     packageNameLineOption: Option<String>
 ) = app.revanced.patches.shared.gms.gmsCoreSupportResourcePatch(
-    fromPackageName = "jp.naver.line.android",
+    fromPackageName = "jp.naver.line.android", // LINEのパッケージ名
     spoofedPackageSignature = "LINEのAPK署名に置き換え", // LINEの正規署名を指定
     gmsCoreVendorGroupIdOption = gmsCoreVendorGroupIdOption,
-    packageNameYouTubeOption = packageNameLineOption,
-    packageNameYouTubeMusicOption = packageNameLineOption,
+    packageNameYouTubeOption = packageNameLineOption, // LINEのパッケージ名を使用
+    packageNameYouTubeMusicOption = packageNameLineOption, // LINEのパッケージ名を使用
     executeBlock = {
         updatePackageName(
-            gmsCoreVendorGroupIdOption.valueOrThrow() + ".android.gms",
-            packageNameLineOption.valueOrThrow()
+            gmsCoreVendorGroupIdOption.valueOrThrow() + ".android.gms", // GMS CoreのベンダーグループIDを更新
+            packageNameLineOption.valueOrThrow() // LINEのパッケージ名を更新
         )
 
         updatePatchStatus("GMSCORE_SUPPORT") // GMSCORE_SUPPORTのステータスを更新
     },
 ) {
     dependsOn(
-        // 必要な依存関係を追加
-        settingsPatch,
-        sharedExtensionPatch
+        settingsPatch, // 必要な依存関係を追加
+        sharedExtensionPatch // LINE用の拡張パッチ
     )
 }
